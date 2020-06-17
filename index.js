6e03b6f366e10e63f64d8688318ff0290f4d40fd
juanWagner <juanswagner@gmail.com>
2020-06-17 14:32:47 -0300

    Routes refening and yalm corrections

--- index.js
+++ index.js
@@ -41,24 +41,6 @@ app.use(url_routes_v1 + "users",
     require(path.join(path_routes_v1, "users.js")));
 
 
-
-// app.post("/login");             // Authenticate PUBLIC
-// app.get("/users");              // Returns all info of user ADM
-// app.post("/users");             // Create a new user USER - COND 1,6
-// app.get("/users/:id");          // Returns info user + hist ranking consumend dishes. USER - COND 6
-// app.put("/users/:id");          // Update user info USER
-// app.post("/users/:id/orders");  // Create a new order for the user USER - COND 3
-// app.get("/orders/:id");  // Return the users' order USER
-// app.put("/orders/:id");  // Update status of the order ADM - COND 4
-// app.get("/dishes");             // List all dishes PUBLIC - COND 2
-// app.post("/dishes");            // Create a plate ADM - COND 5,6
-// app.put("/dishes");             // Modify the plate ADM - COND 5,6
-// app.delete("/dishes");          // Delete a plate ADM - COND 5,6
-// app.get("/orders", (req, res) => {  // List all orders ADM
-//     const { at, before, after } = req.query;
-// });
-
-
 // SQL
 /*
 Favourite plates will be the resulto of the following query
