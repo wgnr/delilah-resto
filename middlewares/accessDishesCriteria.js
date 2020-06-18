0e4d58abeca307efdf26c3fe1ad2f9d397bff72f
juanWagner <juanswagner@gmail.com>
2020-06-18 18:11:55 -0300

    Dish route 2

--- middlewares/accessDishesCriteria.js
+++ middlewares/accessDishesCriteria.js
@@ -1,26 +1,13 @@
 /*
     Validates:
-    * Whether an ID correct param was provided
     * Checking passing criteria
-    
-    Store ID param in res.locals.param_id
 */
 const accessDishesCriteria = (req, res, next) => {
-    // Take id from request's parameter
-    let { id } = req.params;
-
-    // Convert string to number
-    id = +id;
-    if (isNaN(id)) return res.status(401).send("Invalid user ID number.");
-
     /* Check pass criteria:
-        1) Admin see everything.
-        2) User only can query its own information.
+        1) Admin only.
     */
-    if (!res.locals.user.is_admin && res.locals.user.id !== id) return res.sendStatus(401);
+    if (!res.locals.user.is_admin) return res.sendStatus(401);
 
-    // Store param in locas
-    res.locals.param_id = id;
     return next();
 }
 
--- routes/api/v1/dishes.js
+++ routes/api/v1/dishes.js
@@ -16,14 +16,16 @@ const db = require(path.join(__dirname, "..", "..", "..", "db.js"));
 // List all dishes PUBLIC - COND 2
 router.get("/",
     tokenValidator,
-    (req, res) => res.status(201).json(db.Dishes));
+    (req, res) => res.status(201).json(db.Dishes)
+);
 
 // Create a plate ADM - COND 5,6
 router.post("/",
     tokenValidator,
     accessDishesCriteria,
     (req, res) => {
-        const { name, name_short, description, price, img_path, is_available } = req.body;
+        const { name, name_short, description, img_path } = req.body;
+        let { price, is_available } = req.body;
 
         // Price should be a number.
         price = +price;
@@ -42,21 +44,102 @@ router.post("/",
         };
         for (let val in validations) if (validations[val]) return res.status(400).send(validations[val]);
 
-        // {
-        //     "name": "Hamburguesa Clásica",
-        //         "name_short": "HamClas",
-        //             "description": "Hamburguesa 200g de carne, con lechuga y tomate.",
-        //                 "price": 350,
-        //                     "img_path": "./src/img/ham-clas.png",
-        //                         "is_available": true
-        // }
-    });
+        // New Dish
+        const new_dish = {
+            id: Math.round(Math.random() * 1000),
+            name, name_short, description, price, img_path, is_available
+        }
+
+        // Store it in db
+        db.Dishes.push(new_dish);
+
+        return res.status(201).json(new_dish);
+    }
+);
+
+// List all dishes PUBLIC - COND 2
+router.get("/:id",
+    tokenValidator,
+    (req, res) => {
+        // Get id from request's parameter
+        const { id } = req.params;
+
+        // Search id in db
+        const dish = db.Dishes.find(dish => dish.id === +id);
+
+        // If dish doesn't exist, return
+        if (!dish) return res.status(404).send("Dish not found");
+
+        return res.status(200).json(dish);
+    }
+);
 
 // Modify the plate ADM - COND 5,6
-router.put("/", (req, res) => { });
+router.put("/:id",
+    tokenValidator,
+    accessDishesCriteria,
+    (req, res) => {
+        // Get id from request's parameter
+        const { id } = req.params;
+
+        // Search id in db
+        const dish = db.Dishes.find(dish => dish.id === +id);
+
+        // If dish doesn't exist, return
+        if (!dish) return res.status(404).send("Dish not found");
+
+        // Get fields from body
+        const { name, name_short, description, img_path } = req.body;
+        let { price, is_available } = req.body;
+
+        // Price should be a number.
+        price = +price;
+
+        // is_available should be a boolean... in the worst escenario it would be false
+        is_available = is_available === "true" ? true : false;
+
+        // Validate data
+        const validations = {
+            val_name: name && validate.name(name),
+            val_name_short: name_short && validate.name_short(name_short),
+            val_description: description && validate.description(description),
+            val_price: price && validate.price(price),
+            val_img_path: img_path && validate.img_path(img_path),
+            val_is_available: is_available && validate.is_available(is_available)
+        };
+        for (let val in validations) if (validations[val]) return res.status(400).send(validations[val]);
+
+
+        // Update info, skip what wasn't included in body.
+        if (name) dish.name = name;
+        if (name_short) dish.name_short = name_short;
+        if (description) dish.description = description;
+        if (price) dish.price = price;
+        if (img_path) dish.img_path = img_path;
+        if (is_available) dish.is_available = is_available;
+
+        return res.status(200).json(dish);
+    }
+);
 
 // Delete a plate ADM - COND 5,6
-router.delete("/", (req, res) => { });
+router.delete("/",
+    tokenValidator,
+    accessDishesCriteria,
+    (req, res) => {
+        // Get id from request's parameter
+        const { id } = req.params;
+
+        // Search id in db
+        const dish = db.Dishes.find(dish => dish.id === +id);
+
+        // If dish doesn't exist, return
+        if (!dish) return res.status(404).send("Dish not found");
+
+        // Delete dish
+        return res.sendStatus(204);
+    }
+);
 
 module.exports = router;
 
@@ -68,7 +151,7 @@ const validate = {
     },
     name_short: name_short => {
         if (!name_short) return "Empty name_short."
-        if (name.length === 0) return "short_name is too short...";
+        if (name_short.length === 0) return "short_name is too short...";
     },
     description: description => {
         // if (!description) return "Empty description."
