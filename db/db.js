14e2b32ddcd2606bf575f02219ae90c1b2d3199f
juanWagner <juanswagner@gmail.com>
2020-06-23 10:06:01 -0300

    Update tokenvalidator DB

--- db/db.js
+++ db/db.js
@@ -6,6 +6,7 @@ const { sequelize, passphrase } = require(path.join(__dirname, "..", "index.js")
 
 const db = {
     passphrase,
+
     loginAuth: async (username, password) => {
         const user = await sequelize.query(
             "SELECT * FROM `users` where username=:username and password=:password",
@@ -21,7 +22,20 @@ const db = {
         if (user.length !== 1) return;
 
         return { id, id_security_type } = user[0];
+    },
 
+    isTypeAdmin: async (id_sec_type) => {
+        const securityProfile = await sequelize.query(
+            "SELECT type FROM `security_types` where id=:id_sec_type",
+            {
+                type: sequelize.QueryTypes.SELECT,
+                replacements: { id_sec_type }
+            });
+
+        // Check query returns only one result.
+        if (securityProfile.length !== 1) return;
+        
+        return securityProfile[0].type === "Admin" ? true : false;
     }
 }
 
--- middlewares/tokenValidator.js
+++ middlewares/tokenValidator.js
@@ -3,8 +3,8 @@ const path = require("path");
 // JWT Authentication
 const jwt = require("jsonwebtoken");
 
-// Connect 2 db.
-const db = require(path.join(__dirname, "..", "db.js"));
+// DB Methods
+const { isTypeAdmin, passphrase } = require(path.join(__dirname, "..", "db", "db.js"));
 
 
 /*
@@ -15,19 +15,18 @@ const db = require(path.join(__dirname, "..", "db.js"));
     
     Store results in res.locals.user
 */
-const tokenValidatior = (req, res, next) => {
+const tokenValidatior = async (req, res, next) => {
     try {
         // Check whether Bearer authentication token exits in header
         const [bearer, token] = req.headers.authorization.split(" ");
         if (bearer !== "Bearer") return res.status(401).send("Expected Bearer");
 
         // Check whether token is valid
-        const { id, id_security_type } = jwt.verify(token, db.passphrase);
+        const { id, id_security_type } = jwt.verify(token, passphrase);
         if (!id || !id_security_type) return res.status(401).send("id or id_security_type missing in token");
 
         // Check if id is admin
-        const { id: admin_type } = db.Security_Types.find(security => security.type === "Admin")
-        const is_admin = id_security_type === admin_type ? true : false;
+        const is_admin = await isTypeAdmin(id_security_type);
 
         // Store results to locals http://expressjs.com/en/api.html#res.locals
         res.locals.user = { id, is_admin };
